93f88f6479b602805323ff5d1eba7bd3191fa5fb
varsharanitekale <varsharanitekale50@gmail.com>
2021-11-21 22:00:24 +0530

    initial

---
+++ index4.js
@@ -0,0 +1,28 @@
+let n = 5; // you can take input from prompt or change the value
+    let string = "";
+    // Upside pyramid
+    for (let i = 1; i <= n; i++) {
+      // printing spaces
+      for (let j = n; j > i; j--) {
+        string += " ";
+      }
+      // printing star
+      for (let k = 0; k < i * 2 - 1; k++) {
+        string += "*";
+      }
+      string += "\n";
+    }
+    // downside pyramid
+    for (let i = 1; i <= n - 1; i++) {
+      // printing spaces
+      for (let j = 0; j < i; j++) {
+        string += " ";
+      }
+      // printing star
+      for (let k = (n - i) * 2 - 1; k > 0; k--) {
+        string += "*";
+      }
+      string += "\n";
+    }
+    console.log(string);
+    
